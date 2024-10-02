78006b6a3d9011573186703e489d72fcfe198111
rishijain <jainrishi.37@gmail.com>
2024-10-02 17:37:14 +0530

    adjusted code to accept the dynamic field names

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,22 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = current_user.categories.new(category_params)
+    # Initialize the category with the name
+    @category = current_user.categories.new(name: category_params[:name])
+
+    # Parse dynamic fields and create a new hash
+    dynamic_fields = category_params[:dynamic_fields][:field_name]
+    fields_hash = dynamic_fields.each_with_object({}) do |field_name, hash|
+      # Only add non-empty field names to the hash
+      hash[field_name] = "String" unless field_name.blank?
+    end
+
+    debugger
+    # Set the fields hash to the category
+    @category.fields = fields_hash
+
     if @category.save
-      redirect_to @category, notice: 'Category was successfully created.'
+      redirect_to new_category_path, notice: 'Category was successfully created.'
     else
       render :new
     end
@@ -15,6 +28,6 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name, fields: {})
+    params.require(:category).permit(:name, fields: {}, dynamic_fields: {})
   end
 end
